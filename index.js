ca10d8a3fd1ce5036bc727a8ac9fd9f9e45f840d
Snehamay Roy <roysnehamay@gmail.com>
2022-07-01 14:35:01 +0530

    commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app= express();
+ 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+  })
+app.post('/',(req,res)=>{
+    let num1 =Number( req.body.num1);
+    let num2 = Number(req.body.num2);
+    let result = num1 +num2;
+
+    res.send("The sum is "+ result);
+})
+app.listen(3000,() =>{
+    console.log("Server is running on port 3000");
+})
